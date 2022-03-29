f1bc3149cbdb39c03950cf9bb6c83b66e2b97f46
Christian Nikkanen <kisuli@rnd.works>
2022-03-29 21:25:59 +0300

    Add ViteBuild
    Building on the old asset manifest which was built for webpack, this is a succesful attempt at replacing Webpack with Vite.
    
    Documentation and usage info coming soon.

--- src/php/classes/class.asset-manifest.php
+++ src/php/classes/class.asset-manifest.php
@@ -2,17 +2,62 @@
 
 namespace k1;
 
+/**
+ * AssetManifest is for webpack style manifests.
+ */
 class AssetManifest {
-  private $assets = [];
+  public $assets = [];
 
-  public function __construct($path, $name) {
+  // handle prefix
+  public $hp = "k1";
+  public $buildDir = "dist";
+
+  public function __construct(string $path, string $name) {
     $this->assets = (array) json_decode(file_get_contents($path));
     $this->name = $name;
   }
 
+  public function isJS(string $assetName) {
+    return strpos($assetName, '.js') !== false;
+  }
+
+  public function isCSS(string $assetName) {
+    return strpos($assetName, '.css') !== false;
+  }
+
+  public function enqueueJS(string $filename, $dependencies = [], $useVersion = true, $inFooter = true) {
+    $handle = basename($filename);
+    $handle = "{$this->hp}-$handle";
+
+    wp_enqueue_script(
+      $handle,
+      $filename,
+      $dependencies,
+      // Uses the timestamp by default in dev.
+      $useVersion === false ? null : \k1\isDev() ? date('U') : null,
+      $inFooter
+    );
+
+    return $handle;
+  }
+
+  public function enqueueCSS(string $filename, $dependencies = []) {
+    $handle = basename($filename);
+    $handle = "{$this->hp}-$handle";
+
+    wp_enqueue_style(
+      $handle,
+      $filename,
+      $dependencies,
+      \k1\isDev() ? date('U') : null
+    );
+
+    return $handle;
+  }
+
   public function enqueue(string $assetName, $dependencies = []) {
-    $isJS = strpos($assetName, '.js') !== false;
-    $isCSS = !$isJS && strpos($assetName, '.css') !== false;
+    $isJS = $this->isJS($assetName);
+    $isCSS = !$isJS && $this->isCSS($assetName);
     $filename = $this->getAssetFilename($assetName);
 
     if (!$filename) {
@@ -21,39 +66,149 @@ class AssetManifest {
       throw new \Exception($message);
     }
 
-    $basename = basename($filename);
-
     if ($isJS) {
-      wp_enqueue_script(
-        "k1-$basename",
-        $filename,
-        $dependencies,
-        \k1\isDev() ? date('U') : null,
-        true
-      );
+      return $this->enqueueJS($filename, $dependencies, true);
     } else if ($isCSS) {
-      wp_enqueue_style(
-        "k1-$basename",
-        $filename,
-        $dependencies,
-        \k1\isDev() ? date('U') : null
-      );
-    } else {
-      throw new \Exception("Unable to enqueue asset $assetName ($filename) due to type being unsupported");
+      return $this->enqueueCSS($filename);
     }
 
-    // Return the handle for use in wp_localize_script
-    return "k1-$basename";
+    throw new \Exception("Unable to enqueue asset $assetName ($filename) due to type being unsupported");
+  }
+
+  public function getAsset(string $assetName) {
+    $asset = $this->assets[$assetName];
+
+    return !empty($asset) ? $asset : false;
+  }
+
+  /**
+   * The asset manifest doesn't know of WP, it assumes the files are available in webroot.
+   */
+  public function withBuildDirectory(string $filename, $forBrowser = true) {
+    return ($forBrowser ? \get_stylesheet_directory_uri() : \get_stylesheet_directory()) . "/{$this->buildDir}/$filename";
   }
 
   public function getAssetFilename(string $assetName, $forBrowser = true) {
-    if (isset($this->assets[$assetName])) {
+    $asset = $this->getAsset($assetName);
+
+    if ($asset) {
       $filename = $this->assets[$assetName];
 
-      return ($forBrowser ? \get_stylesheet_directory_uri() : \get_stylesheet_directory()) . "/dist/$filename";
+      return $this->withBuildDirectory($filename, $forBrowser);
     }
 
     return false;
   }
 
+}
+
+class ViteBuild extends AssetManifest {
+  public $buildDir = "vitebuild";
+  public $serverUrl = "http://localhost:8888";
+  public $serverPort = 8888;
+  protected $dev = false;
+
+  public function __construct(string $path, string $name) {
+    parent::__construct($path, $name);
+
+    if ($this->devServerExists()) {
+      $this->dev = true;
+
+      add_action('wp_enqueue_scripts', function() {
+        // This doesn't use the internal enqueue so it can set the script handle freely.
+        wp_enqueue_script("vite/client", $this->serverUrl . "/@vite/client", [], null, false);
+      }, 1);
+    }
+  }
+
+  public function isDev() {
+    return $this->dev;
+  }
+
+  public function getAssetFilename(string $assetName, $forBrowser = true) {
+    $asset = $this->getAsset($assetName);
+
+    if ($asset) {
+        $filename = $asset->file;
+
+        return $this->withBuildDirectory($filename);
+      }
+
+      return false;
+  }
+
+  public function enqueueJS(string $filename, $dependencies = [], $useVersion = true, $inFooter = true) {
+    $handle = parent::enqueueJS($filename, $dependencies, $useVersion, $inFooter);
+
+    add_filter('script_loader_tag', function($tag, $h, $src) use ($handle) {
+      if ($handle !== $h) {
+        return $tag;
+      }
+
+      $tag = '<script id="' . esc_attr($handle) . '" type="module" src="' . esc_url($src) . '"></script>';
+      return $tag;
+    } , 10, 3);
+
+    return $handle;
+  }
+
+  public function enqueue(string $assetName, $dependencies = [], $options = []) {
+    $useCSS = $options['useCSS'] ?? false;
+    $isJS = $this->isJS($assetName);
+
+    if (!$this->isDev()) {
+      $filename = $this->getAssetFilename($assetName);
+    } else {
+      $filename = $this->serverUrl . '/' . $assetName;
+    }
+
+    if (!$filename) {
+      $message = "Unable to enqueue asset $assetName. It wasn't present in the {$this->name} manifest. ";
+
+      throw new \Exception($message);
+    }
+
+    if ($useCSS) {
+      $asset = $this->getAsset($assetName);
+      $files = $asset->css;
+
+      $handles = [];
+
+      foreach ($files as $i => $css) {
+        $filename = $this->withBuildDirectory($css);
+        $handles = $this->enqueueCSS($filename);
+      }
+
+      return $handles;
+    } else if ($isJS) {
+      return $this->enqueueJS($filename, $dependencies, false, true);
+    }
+
+    throw new \Exception("Unable to enqueue asset $assetName ($filename) due to type being unsupported");
+  }
+
+  /**
+   * This is the best way I found to check for the existence.
+   *
+   * fopen throws warnings if it can't find whatever it's looking for. That's why I used the STFU operator,
+   * because those warnings are irrelevant. They're expected in prod.
+   */
+  private function devServerExists(){
+    $file = @fopen($this->serverUrl . "/@vite/client", "r");
+
+    if (!$file) {
+      // If WP is running inside Docker but Vite is running on the host, retrying with this should work.
+      // This should work on Mac & Linux. It does not work from the client!
+      $file = @fopen("http://host.docker.internal:{$this->serverPort}/@vite/client", "r");
+
+      if (!$file) {
+        return false;
+      }
+
+      return true;
+    }
+
+    fclose($file);
+    return true;
+  }
 }
